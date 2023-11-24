6e3f3aba354265d728dc218c7c110e9915dd55b0
Richard Wang <62804488+ricky9w@users.noreply.github.com>
2023-11-24 20:34:36 +0800

    delete version limit for python-telegram-bot

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     install_requires=[
         "ehforwarderbot>=2.0.0b28",
-        "python-telegram-bot>=10.0.0<12.0.0",
+        "python-telegram-bot>=10.0.0",
         "python-magic",
         "peewee",
         "PyYaml",
